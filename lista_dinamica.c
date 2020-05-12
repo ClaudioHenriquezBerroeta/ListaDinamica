1fc962f5a46d10ffcd5b4df216719c0dd1e3e042
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-05-12 17:13:42 -0400

    Update lista_dinamica.c

--- lista_dinamica.c
+++ lista_dinamica.c
@@ -1,2 +1,17 @@
-struct nodo { tipo_elem elem;
-struct nodo *sgte; };
+#include <stdlib.h>
+struct nodo { 
+	int dato;
+	struct nodo *sgte;
+};
+
+struct nodo *constructor(void){
+	struct nodo *L = (struct nodo *)malloc(sizeof(struct nodo)); 
+	L->sgte=NULL;
+	return L;
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
