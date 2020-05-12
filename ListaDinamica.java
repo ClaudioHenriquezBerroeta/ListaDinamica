5130cd0d1870d7386a398d3e14ffb4a4455dfdcc
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-05-12 17:13:36 -0400

    Update ListaDinamica.java

--- ListaDinamica.java
+++ ListaDinamica.java
@@ -1,3 +1,37 @@
 class ListaDinamica{
-	
+	NodoLista cabeza;
+
+	ListaDinamica(){
+		cabeza = new NodoLista();
+		cabeza.sgte=null;
+	}
+
+	public void insertar(int x, int p){
+		NodoLista aux=new NodoLista(x);
+		NodoLista temp=cabeza;
+		int i=0;
+		if(p<0) System.out.println("Posicion invalida!");
+		else{
+			while(temp.sgte!= null){
+				System.out.println("entró al while");
+				if(i==p){//encontré la posición
+					System.out.println("encontró la posicion");
+					insertar(aux,temp);
+				}
+				i++;
+				temp=temp.sgte;
+			}
+			if(i<p) System.out.println("La posición indicada no existe!");
+			insertar(aux,temp);
+		}
+				
+
+	}
+	private void insertar(NodoLista x,NodoLista p){
+			x.sgte=p.sgte;
+			p.sgte=x;
+	}
+	//public mostrar()
+
+
 }
