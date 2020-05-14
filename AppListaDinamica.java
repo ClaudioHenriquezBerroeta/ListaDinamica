832f4b2619bca8eedf18b7bb37e4877b3fdf4c81
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-05-14 12:03:36 -0400

    Update AppListaDinamica.java

--- AppListaDinamica.java
+++ AppListaDinamica.java
@@ -1,9 +1,10 @@
 class AppListaDinamica{
 	public static void main(String[] args) {
 		ListaDinamica L = new ListaDinamica();
-		L.insertar(10,0);
+		L.insertar(10,6);
 		L.insertar(20,1);
-		L.insertar(30,2);
-		System.out.println(L.cabeza.sgte.dato);
+		L.insertar(30,0);
+		L.mostrar();
+		//System.out.println(L.cabeza.sgte.dato);
 	}
 }
